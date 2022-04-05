d40b2f25dad8ccdbdfec3b67832e5cf066bfe530
kkatayama <kkatayama@udel.edu>
2022-04-05 01:47:37 -0400

    patch makeover 1.3

--- server.py
+++ server.py
@@ -46,7 +46,7 @@ def add(db, table="", url_paths=""):
 
     # -- check for required parameters
     if any(k not in params.keys() for k in required_columns):
-        res = {"message": "missing paramaters", "required": required_columns, "supplied": request.params|params}
+        res = {"message": "missing paramaters", "required": required_columns, "supplied": {"params": params}
         return clean(res)
 
     # -- the users table requires additional formatting and checking
@@ -139,7 +139,7 @@ def edit(db, table="", url_paths=""):
     # -- at least 1 edit parameter required
     if not any(k in params.keys() for k in editable_columns):
         res = {"message": "missing a parameter to edit",
-               "editable": extract(col_info, editable_columns), "supplied": request.params}
+               "editable": extract(col_info, editable_columns), "supplied": {"params": params}}
         return clean(res)
 
     # -- at least 1 query parameter required
@@ -148,7 +148,7 @@ def edit(db, table="", url_paths=""):
     query_params = non_edit_columns + ["filter"]
     if not any(k in supplied.keys() for k in query_params):
         res = {"message": "missing a query parameter",
-               "query_params": extract(col_info, query_params), "supplied": supplied}
+               "query_params": extract(col_info, query_params), "supplied": {'params': params, 'filter': filters}}
         return clean(res)
 
     # -- define "columns" to edit and "values" to insert (parsed from params in HTTP request)
@@ -179,7 +179,7 @@ def edit(db, table="", url_paths=""):
         message = f"0 {table.rstrip('s')} entries found matching your parameters"
 
     # -- send response message
-    res = {"message": message, "supplied": supplied}
+    res = {"message": message, "supplied": {'params': params, 'filter': filters}}
     return clean(res)
 
 ###############################################################################
