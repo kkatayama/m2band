966994ae825746d71883a13d7843197245fbc5e2
kkatayama <kkatayama@udel.edu>
2022-04-05 00:18:35 -0400

    patch makeover 1.2

--- db_functions.py
+++ db_functions.py
@@ -457,7 +457,7 @@ def clean(data):
     str_data = json.dumps(data, default=str, indent=2)
     if re.search(regex, user_agent):
         print(str_data)
-        return(str_data.replace('\n', '\n<br />'))
+        return template('<pre> {{str_data}} </pre>', str_data=str_data)
 
     cleaned = json.loads(str_data)
     print(cleaned)
