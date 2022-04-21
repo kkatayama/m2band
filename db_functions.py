963f8f38b2869d587236deb707d538df74e219b7
kkatayama <kkatayama@udel.edu>
2022-04-21 12:22:50 -0400

    added bottle-errorsrest

--- db_functions.py
+++ db_functions.py
@@ -620,6 +620,7 @@ def log_to_logger(fn):
                                         request.method,
                                         request.url,
                                         response.status))
+        logger.info(f'type(actual_response) = {type(actual_response)}')
         if isinstance(actual_response, dict):
             if not actual_response.get("message") == "available commands":
                 logger.info(json.dumps({"request.params": dict(request.params)}))
